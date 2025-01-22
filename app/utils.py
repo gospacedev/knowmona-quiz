93fca1d963bc5c04670ce3a354ae7d99018bef17
Grantley Cullar <grantcullar@gmail.com>
2025-01-22 08:52:52 +0800

    Update homepage templates

--- app/utils.py
+++ app/utils.py
@@ -48,12 +48,14 @@ def infer_quiz_json(form, uploaded_texts=""):
         if uploaded_texts == "":
             uploaded_texts = "No user inputted information"
 
-        instructions_prompt = f"""<s> [INST] Your are a great teacher and your task is to create 10 questions with 4 choices with a {question_difficulty} difficulty in a {tone} tone about {topic}, then create an answers. Index in JSON format, the questions as "Q#":"" to "Q#":"", the four choices as "choice_1" to "choice_4", the answers as "choice_1" to "choice_4", and a one-sentence explanation. WRITE NOTHING ELSE DO AND NOT REPEAT QUESTIONS Ulitize some of these information from the Internet:  """ + external_data + """\nUser inputted information: """ + uploaded_texts + f"""[/INST]"""
+        instructions_prompt = f"""<s> [INST] Your are a great teacher and your task is to create 10 questions with 4 choices with a {question_difficulty} difficulty in a {tone} tone about {topic}, then create an answers. Index in JSON format, the questions as "Q#":"" to "Q#":"", the four choices as "choice_1" to "choice_4", the answers as "choice_1" to "choice_4", and a one-sentence explanation. Ulitize  these information from the Internet, for only three questions, ONLY IF THEY ARE RELEVANT:  """ + external_data + """\nUser inputted information: """ + uploaded_texts + f""" WRITE NOTHING ELSE DO AND NOT REPEAT QUESTIONS [/INST]"""
 
         response = client.chat.completions.create(
             model="meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo",
             max_tokens=4096,
-            temperature=0.3,
+            temperature=0.7,
+            top_p=0.7,
+            top_k=50,
             repetition_penalty=1,
             messages=[{"role": "system", "content": instructions_prompt}],
             response_format={
@@ -63,8 +65,6 @@ def infer_quiz_json(form, uploaded_texts=""):
         )
 
         output = response.choices[0].message.content
-
-        print(json.dumps(output, indent=4))
 
         return json.loads(output), external_reference
     else:
@@ -148,4 +148,3 @@ def save_quiz_from_json(quiz_data, external_reference, quiz):
             quiz=quiz,
             defaults={'text': external_reference}
         )
-
